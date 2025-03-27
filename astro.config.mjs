124205b27feb3b146af4ab04ba5c377e86db1404
Anton Sten <anton@lepetitgarcon.com>
2025-03-27 14:44:05 +0100

    Add Netlify adapter for server-side rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,6 +17,7 @@ export default defineConfig({
     })
   ],
   output: 'server',
+  adapter: netlify(),
   markdown: {
     shikiConfig: {
       theme: 'github-dark'
