f9186450f03de36e1159f9ba34be4318416fa60b
Anton Sten <anton@lepetitgarcon.com>
2025-05-11 17:31:10 +0200

    Fix RSS feed to use correct article publish dates and update project files

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -10,7 +10,7 @@ export async function GET(context) {
     site: context.site,
     items: articles.map(article => ({
       title: article.data.title,
-      pubDate: article.data.pubDate,
+      pubDate: article.data.date,
       description: article.data.description,
       link: `/articles/${article.slug}/`,
       content: article.body,
