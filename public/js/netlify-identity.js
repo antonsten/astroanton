fa49a1b47d150796e15877c503e08741ea1208bf
Anton Sten <anton@lepetitgarcon.com>
2025-05-16 14:56:50 +0200

    Improve performance and fix CORS issues - Add critical CSS inlining for faster initial render - Optimize image loading with responsive srcset - Fix Netlify Identity Widget CORS issues - Defer non-critical CSS and scripts

---
+++ public/js/netlify-identity.js
@@ -0,0 +1,8 @@
+// Netlify Identity Widget
+(function() {
+  var script = document.createElement('script');
+  script.src = 'https://identity.netlify.com/v1/netlify-identity-widget.js';
+  script.crossOrigin = 'anonymous';
+  script.defer = true;
+  document.head.appendChild(script);
+})(); 
