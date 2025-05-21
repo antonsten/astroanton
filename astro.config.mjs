113a67a43d834a6b52352f8f93bb5696ad1b7902
Anton Sten <anton@lepetitgarcon.com>
2025-05-21 13:56:54 +0200

    Update Netlify adapter import to use standard adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
-import netlify from '@astrojs/netlify/static';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
